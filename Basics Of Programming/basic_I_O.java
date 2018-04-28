1e4dca5728bf4ad47c1ec4bbf7fff59cd1fd8bd8
Dhruv Chamania <dhruv.chamania96@gmail.com>
2018-04-28 19:32:36 +0530

    Update basic_I_O.java

--- Basics Of Programming/basic_I_O.java
+++ Basics Of Programming/basic_I_O.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+      
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        in.nextLine();
+        String name = in.nextLine();                 
+        System.out.println(num*2);
+        System.out.print(name);
+
+     
+    }
+}
 
