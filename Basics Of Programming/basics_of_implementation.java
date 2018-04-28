fbae0542d0b18d86d5c4994657b0e904c04ec9f0
Dhruv Chamania <dhruv.chamania96@gmail.com>
2018-04-28 20:41:26 +0530

    BigInteger
    Capable of Handling bigger numbers

--- Basics Of Programming/basics_of_implementation.java
+++ Basics Of Programming/basics_of_implementation.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.math.*;
 
-class TestClass {
+class Simple {
     public static void main(String args[] ) throws Exception {
     
     Scanner in = new Scanner(System.in);
-    int num = in.nextInt();
+    BigInteger num = in.nextBigInteger();
+    BigInteger f = new BigInteger("0");
+    BigInteger bi1,bi3;
+
+    bi1 = new BigInteger("10");
+   
     int remainder = 0;
     int[] digits = new int[10];
-    while(num>0){
-        remainder = num % 10;
+    while(num.compareTo(f) > 0){
+    	bi3 = num.mod(bi1);
+        remainder =  bi3.intValue();
         digits[remainder]++;
-        num = num/10;
+        num = num.divide(bi1);
     }
     
     for (int counter = 0; counter<10;counter ++){
