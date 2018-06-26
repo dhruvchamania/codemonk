62af37f9f1add89a8d673d4df37cb12135217b7e
dhruvchamania <dhruv.chamania96@gmail.com>
2018-06-26 11:46:18 +0530

    PrimeWithSquare

--- Math/PrimalityTest.java
+++ Math/PrimalityTest.java
@@ -1,1 +1,36 @@
 
+import java.util.*;
+import java.lang.*;
+
+class testcase {
+     
+    static boolean isprime(long n)
+    {
+    
+        if (n <= 1)  return false;
+        if (n <= 3)  return true;
+        if (n%2 == 0 || n%3 == 0) return false;
+ 
+    for (long i=5; i*i<=n; i=i+6)
+        if (n%i == 0 || n%(i+2) == 0)
+           return false;
+ 
+    return true;
+    }
+    
+    public static void main(String args[] ) throws Exception {
+        Scanner s = new Scanner(System.in);
+        long n = s.nextLong();
+        int counter =0;
+        for(int i = 0; i<n; i++){
+            long chk;
+            chk = s.nextInt();
+            if(isprime(chk))
+            System.out.println("prime");
+            else
+            System.out.println("composite");
+            } 
+          
+    }
+    
+}
